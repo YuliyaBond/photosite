21e6d9ef36e38a347113d03a0ef30c2abbc13e9a
Feboll <klaus24128@gmail.com>
2018-10-09 15:45:59 +0300

    Исправление слайдера

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,13 @@
-
-var slides = document.querySelectorAll('.slider__content .item-slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000); 
-
-function nextSlide() {
- slides[currentSlide].className = 'item-slide';
- currentSlide = (currentSlide+1)%slides.length;
- slides[currentSlide].className = 'item-slide showing';
-}
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    var slides = document.querySelectorAll('.slider__content .item-slide');
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide, 5000); 
+    
+    function nextSlide() {
+    slides[currentSlide].className = 'item-slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'item-slide showing';
+    }
+  }
+};
